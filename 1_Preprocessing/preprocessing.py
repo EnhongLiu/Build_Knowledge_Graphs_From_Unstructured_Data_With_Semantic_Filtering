7bd203b9ad1cb5adf61d08a2e3e910a30d7cc964
EnhongLiu <el839@cornell.edu>
2025-02-25 13:46:25 -0700

    Update preprocessing.py

--- 1_Preprocessing/preprocessing.py
+++ 1_Preprocessing/preprocessing.py
@@ -2,6 +2,11 @@ import pandas as pd
 import numpy as np
 import re
 from typing import List
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import Document
+import pprint
+import uuid
+
 
 # Read in data
 jds_metadata = pd.read_csv('./jds_metadata.csv')
@@ -27,4 +32,46 @@ journal_df = pd.concat([jds_cleaned,animal_cleaned])[['Title','Pure Abstract','A
 
 journal_df_cleaned = journal_df.dropna(subset=['Pure Abstract'], how='any')
 
-# print(journal_df_cleaned.shape)
+
+
+# Convert the extracted text into Document objects
+documents = [
+    Document(
+        page_content=row['Pure Abstract'], 
+        metadata={col: row[col] for col in jds_test.columns if col != 'Pure Abstract'}
+    )
+    for _, row in jds_test.iterrows()
+]
+
+# Initialize the text splitter
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=600,
+    chunk_overlap=100,
+    length_function=len,
+    is_separator_regex=False,
+)
+
+# Split the documents into chunks
+chunks = splitter.split_documents(documents)
+
+
+
+def documents2Dataframe(documents):
+    rows = []
+    for chunk in documents:
+        row = {
+            "text": chunk.page_content,
+            **chunk.metadata,
+            "chunk_id": uuid.uuid4().hex,
+        }
+        rows = rows + [row]
+
+    df = pd.DataFrame(rows)
+    return df
+
+
+df_chunks = documents2Dataframe(chunks)
+
+print(df_chunks.shape)
+# Output the DataFrame
+display(df_chunks.head())  
